8fb2f17a70cd3d592c594bebaaf4cd77bb53f798
Zachary <Zachary@admiralfeb.net>
2021-02-21 22:44:35 -0600

    init

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        var abbr = string.Empty;
+        var delimiters = new char[] { ' ', '-', '_' };
+
+        foreach (var word in phrase.Split(delimiters, StringSplitOptions.RemoveEmptyEntries))
+        {
+            abbr += word[0].ToString().ToUpper();
+        }
+
+        return abbr;
+    }
+}
---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        var wordArr = word.ToUpper().ToCharArray();
+        Array.Sort(wordArr);
+        char previousLetter = '-';
+        foreach (var letter in wordArr)
+        {
+            if (Char.IsLetter(letter))
+            {
+                if (Char.IsLetter(letter) && letter == previousLetter)
+                {
+                    return false;
+                }
+                previousLetter = letter;
+            }
+        }
+        return true;
+    }
+}
