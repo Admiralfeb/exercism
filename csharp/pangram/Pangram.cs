e712118520487d0d996019232b2036487188213b
Zachary Bryant <Zachary@admiralfeb.net>
2021-02-21 23:09:04 -0600

    C# leap and pangram complete

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        var alphabet = new List<char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        var lowerInput = input.ToLower();
+        foreach (var letter in alphabet)
+        {
+            if (!lowerInput.Contains(letter))
+                return false;
+        }
+        return true;
     }
 }
