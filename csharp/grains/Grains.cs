b7d84bb5b85ce6db14f2e7e96b4e12070714986a
Zachary Bryant <Zachary@admiralfeb.net>
2021-02-22 22:22:08 -0600

    more c#

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n < 1 || n > 64)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        return Convert.ToUInt64(Math.Pow(2, n - 1));
+    }
+
+    public static ulong Total()
+    {
+        UInt64 total = 0;
+        for (int i = 1; i < 65; i++)
+        {
+            total += Square(i);
+        }
+        return total;
+    }
+}
---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        var reversed = string.Empty;
+        foreach (var ch in input)
+            reversed = ch + reversed;
+
+        return reversed;
+    }
+}
